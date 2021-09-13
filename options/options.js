2dac9d7e72e0285f20ab09217b4b2d33724781ed
Crowdbotics <team@crowdbotics.com>
2021-09-13 19:07:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mayank_test_30518Identifier",
+  url: "https://mayank_test_30518Identifier.botics.co",
+  api: "https://mayank_test_30518Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
